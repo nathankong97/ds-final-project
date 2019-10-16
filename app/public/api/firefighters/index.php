7b1569c52cc163e2086227841dcd621c12fdaa3b
Nathan <fanyangkong97@gmail.com>
2019-10-16 19:29:21 -0400

    connect members to php

--- app/public/api/firefighters/index.php
+++ app/public/api/firefighters/index.php
@@ -1,20 +1,16 @@
 <?php
+
 // Step 1: Get a datase connection from our help class
 $db = DbConnection::getConnection();
+
 // Step 2: Create & run the query
-if (isset($_GET['guid'])) {
-  $stmt = $db->prepare(
-    'SELECT * FROM Patient
-    WHERE patientGuid = ?'
-  );
-  $stmt->execute([$_GET['guid']]);
-} else {
-  $stmt = $db->prepare('SELECT * FROM Patient');
-  $stmt->execute();
-}
-$patients = $stmt->fetchAll();
+$stmt = $db->prepare('SELECT * FROM Member');
+$stmt->execute();
+$members = $stmt->fetchAll();
+
 // Step 3: Convert to JSON
-$json = json_encode($patients, JSON_PRETTY_PRINT);
+$json = json_encode($members, JSON_PRETTY_PRINT);
+
 // Step 4: Output
 header('Content-Type: application/json');
 echo $json;
