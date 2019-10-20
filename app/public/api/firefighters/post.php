63f35ae3b3dce2ecfbaebeb374d261fcd6647698
Arlie Cobb <cobbarl@iu.edu>
2019-10-20 12:45:25 -0400

    Create post. php for firefighters and guids
    Created guids and teh post api file for firefighters

---
+++ app/public/api/firefighters/post.php
@@ -0,0 +1,42 @@
+<?php
+//Step 0:Validate incoming datase (Arlie entered)
+use Ramsey\Uuid\Uuid;
+
+$uuid4= Uuid4();
+$guid= $uuid4->toSring(); //i.e. 25786-5859486-4485...
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection:: getConnection;
+$Statement
+// Step 2: Create & run the query
+$stmt =$db -> Prepare (
+  'INSERT INTO Member
+    (memberGuid, firstName, lastName, position, sexAtBirth, address, workPhone, radioNumber, stationNumber,  isActive, dob, startDate)
+  VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)'
+);
+
+$stmt-> execute ([
+  $guid, //ie 257......
+  $_POST ['firstName'],
+  $_POST ['lastName'],
+  $_POST ['position'],
+  $_POST ['sexAtBirth'],
+  $_POST ['address'],
+  $_POST ['workPhone'],
+  $_POST ['workPhone'],
+  $_POST ['radioNumber'],
+  $_POST ['stationNumber'],
+  $_POST ['isActive'],
+  $_POST ['dob'],
+  $_POST ['startDate']
+]);
+
+//Step 4: fann_descale_output
+header('HTTO/1.1 303 See Other');
+header('Location: ../firefighters/?guid='$guid);//<- pulls only one record (dots connect strings so 'loaction' is a string and $guid is a string)
+/*Our database needs a primary key= Globally unique identifier GUID
+he googled php create guid*/
+
+
+
+ ?>
