8e515137dae5aa71fbe8060071cb8d6277ff2579
guptasan98 <guptasan@iu.edu>
2019-10-20 20:30:35 -0400

    made changes to post php
    still not getting the post function to work

--- app/public/api/firefighters/post.php
+++ app/public/api/firefighters/post.php
@@ -1,5 +1,45 @@
 <?php
+//Step 0:Validate incoming datase (Arlie entered)
+use Ramsey\Uuid\Uuid;
 
+
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection:: getConnection;
+//$Statement
+// Step 2: Create & run the query
+$stmt =$db -> Prepare (
+  'INSERT INTO Member
+    (memberGuid, firstName, lastName, position, sexAtBirth, address, workPhone, radioNumber, stationNumber,  isActive, dob, startDate)
+  VALUES (?,?,?,?,?,?,?,?,?,?,?,?)'
+);
+
+$uuid4= Uuid4();
+$guid= $uuid4->toSring(); //i.e. 25786-5859486-4485...
+//$guid = Uuid::uuid4()->toString();
+
+$stmt-> execute ([
+  $guid, //ie 257......
+  $_POST ['firstName'],
+  $_POST ['lastName'],
+  $_POST ['position'],
+  $_POST ['sexAtBirth'],
+  $_POST ['address'],
+  $_POST ['workPhone'],
+  $_POST ['radioNumber'],
+  $_POST ['stationNumber'],
+  $_POST ['isActive'],
+  $_POST ['dob'],
+  $_POST ['startDate']
+]);
+
+//Step 4: fann_descale_output
+header('HTTO/1.1 303 See Other');
+header('Location: ../firefighters/?guid='.$guid);//<- pulls only one record (dots connect strings so 'location' is a string and $guid is a string)
+/*Our database needs a primary key= Globally unique identifier GUID
+he googled php create guid*/
+
+/*
 // Step 0: Validation
 use Ramsey\Uuid\Uuid;
 $guid = Uuid::uuid4()->toString(); // i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a
@@ -9,7 +49,7 @@ $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
 $stmt = $db->prepare(
-  'INSERT INTO Member 
+  'INSERT INTO Member
   (memberGuid, firstName, lastName, position, sexAtBirth, address, workPhone, radioNumber, stationNumber, isActive, dob, startDate)
   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
 );
@@ -31,3 +71,5 @@ $stmt->execute([
 
 // Step 4: Output
 header('HTTP/1.1 303 See Other');
+*/
+ ?>
