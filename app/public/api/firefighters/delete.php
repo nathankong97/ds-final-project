e71bedbedb32b7b16565ea8dd70f1ab4d9ec9622
Nathan <fanyangkong97@gmail.com>
2019-10-24 18:10:38 -0400

    add delete member function

--- app/public/api/firefighters/delete.php
+++ app/public/api/firefighters/delete.php
@@ -1,34 +1,14 @@
 <?php
-//Step 0:Validate incoming datase (Arlie entered)
-use Ramsey\Uuid\Uuid;
 
-// Step 1: Get a datase connection from our help class
+
 $db = DbConnection::getConnection();
-
-// Step 2: Create & run the query
 $stmt = $db->prepare(
-  'INSERT INTO Member
-  (memberGuid, firstName, lastName, position, sexAtBirth, address, workPhone, radioNumber, stationNumber, isActive, dob, startDate)
-  VALUES (?,?,?,?,?,?,?,?,?,?,?,?)'
-);
+  'DELETE FROM Member
+  WHERE memberGuid = ?');
 
-$guid = Uuid::uuid4()->toString();
 
-$stmt->execute([
-  $guid, //ie 257......
-  $_POST['firstName'],
-  $_POST['lastName'],
-  $_POST['position'],
-  $_POST['sexAtBirth'],
-  $_POST['address'],
-  $_POST['workPhone'],
-  $_POST['radioNumber'],
-  $_POST['stationNumber'],
-  $_POST['isActive'],
-  $_POST['dob'],
-  $_POST['startDate']
-]);
+  $stmt->execute([
+    $_POST['memberGuid']
+  ]);
 
-//Step 4: fann_descale_output
-header('HTTP/1.1 303 See Other');
-header('Location: ../firefighters/?guid='.$guid);
+  header('HTTP/1.1 303 See Other');
--- app/public/js/members.app.js
+++ app/public/js/members.app.js
@@ -18,23 +18,25 @@ var membersApp = new Vue({
           "Content-Type": "application/json; charset=utf-8"
         }
       })
-      .then( response => response.json())
-      .then( json => {membersApp.members= json[0] })
+      window.alert("Member was created");
+      window.location.href = 'members.html';
     },
     handleClick(member) { //check if row is needed here//
       memberDetailApp.member = member;
     },
-    handleEdit(member) {
-      membersApp.member = member;
+    handleEdit(m) {
+      window.location.href = 'editmember.html?memberGuid='+m.memberGuid;
     },
-    handleDelete(event) {
-      fetch('api/firefighters/post.php', {
+    handleDelete(m) {
+      fetch('api/firefighters/delete.php', {
         method: 'POST',
-        body: JSON.stringify(this.m),
+        body: JSON.stringify(m),
         headers: {
           "Content-Type": "application/json; charset=utf-8"
         }
       })
+      window.alert("Member was deleted");
+      window.location.href = 'members.html';
     }
   },
   created() {
