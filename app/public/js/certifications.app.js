58ef1496483f0c20395c5466f60432e922d20e90
guptasan98 <guptasan@iu.edu>
2019-10-26 16:39:14 -0400

    partial work

--- app/public/js/certifications.app.js
+++ app/public/js/certifications.app.js
@@ -1,13 +1,25 @@
 var certificationsApp = new Vue({
   el: '#certificationsApp',
   data: {
-    certifications: []
+    certifications: [],
+    certificate: {}
   },
   methods: {
     fetchCertifications() {
       fetch('api/certifications/index.php')
       .then(response => response.json())
       .then(json => { certificationsApp.certifications = json })
+    },
+    handleSubmit() {
+      fetch('api/certifications/post.php', {
+        method: 'POST',
+        body: JSON.stringify(this.certificate),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      window.alert("Certificate was created");
+      window.location.href = 'certification.html';
     }
   },
   created() {
