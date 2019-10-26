c111670bc81ee1c5a1f857e2b844455557d30fd1
guptasan98 <guptasan@iu.edu>
2019-10-26 13:38:06 -0400

    Update fireGuyReport.app.js

--- app/public/js/fireGuyReport.app.js
+++ app/public/js/fireGuyReport.app.js
@@ -1,14 +1,31 @@
 var fireGuyReport = new Vue({
   el: '#fireGuyReport',
   data: {
-    Firefighters: []
+    Firefighters: [],
+    firefighter:{}
   },
   methods: {
     fetchFirefighters() {
       fetch('api/reports/firefighters.php')
       .then(response => response.json())
       .then(json => { fireGuyReport.Firefighters = json })
-    }
+    },
+//changes made below
+    handleSubmitFFdetails(event) {
+          fetch('api/reports/firefightersFiltered.php', {
+            method: 'POST',
+            body: JSON.stringify(Firefighter.stationNumber),
+            headers: {
+              "Content-Type": "application/json; charset=utf-8"
+            }
+          })
+          .then(response => response.json())
+          .then(json => { fireGuyReport.Firefighters = json })
+          window.alert("Report Filtered");
+        }
+
+//changes made above
+
   },
   created() {
     this.fetchFirefighters();
